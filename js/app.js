5e0b351032f2724cd577ec1d407898080fe529ba
Andres <andres@lamillac.com>
2016-05-04 08:03:37 +0200

    ejercicio 4

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var ViewModel = function() {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('');
+    this.nicknames = ko.observableArray(["Tabtab", "T-Bone", "Mr. T", "Tabitha Tab Tabby Catty Cat"]);
     this.level = ko.computed(function() {
         console.log("Funcion ejecutada");
         var level = "Teen";
