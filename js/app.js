47d58e09247b51864862da4ac0d62d68f0580d6b
Andres <andres@lamillac.com>
2016-05-04 07:53:29 +0200

    Ejercicio 3

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgAttribution = ko.observable('');
+    this.level = ko.computed(function() {
+        console.log("Funcion ejecutada");
+        var level = "Teen";
+        if(this.clickCount() < 10) {
+            level = "Newborn";
+        } else if (this.clickCount() < 100) {
+            level = "Infant";
+        }
+        return level;
+    }, this);
+
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+}
+
+ko.applyBindings(new ViewModel());
